d1e39a59ad202a2848045c6446343876cb26becd
Julian Benalcazar <oswaldobenal12@gmail.com>
2024-03-19 18:11:43 -0500

    nextjs my dashboard

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,16 @@
+import { CardCounter } from "@/app/shopping-cart/components";
+
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "Un simple contador",
+};
+
+export default function CounterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CardCounter value={20}/>
+    </div>
+  );
+}
